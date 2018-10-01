7d61254ac20c7e02e708e5b48b9aa9555635c10d
Anthony Scopatz <scopatz@gmail.com>
2018-10-01 18:37:38 -0400

    bumped version to 0.2.5

--- bash_completion.py
+++ bash_completion.py
@@ -14,7 +14,7 @@ import platform
 import functools
 import subprocess
 
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 
 
 @functools.lru_cache(1)
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     HAVE_SETUPTOOLS = False
 
 
-VERSION = '0.2.4'
+VERSION = '0.2.5'
 
 setup_kwargs = {
     "version": VERSION,
